d6c67ff9a8d6a3519f33991a1e1be15c1208a1b7
Andrew Pattison <andygjp@hotmail.com>
2019-05-02 23:38:48 +0100

    feat: moving onto tens

--- RomanNumerals.StringReplace/UnitTest1.cs
+++ RomanNumerals.StringReplace/UnitTest1.cs
@@ -12,6 +12,10 @@ namespace RomanNumerals.StringReplace
         [InlineData(3, "III")]
         [InlineData(4, "IV")]
         [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
         public void Number_should_convert_to_expected_numeral(int input, string expected)
         {
             var actual = Convert(input);
@@ -23,6 +27,7 @@ namespace RomanNumerals.StringReplace
         {
             var convert = new StringBuilder()
                 .Append('I', input)
+                .Replace("IIIIIIIII", "IX")
                 .Replace("IIIII", "V")
                 .Replace("IIII", "IV")
                 .ToString();
